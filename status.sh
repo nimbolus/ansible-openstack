5cb601d4985b62a24e46d0b53668a04af738be59
Lukas Steiner <lukas.steiner@steinheilig.de>
2019-08-14 17:07:19 +0200

    fix mixing roles

--- status.sh
+++ status.sh
@@ -2,5 +2,5 @@
 
 ansible controller_nodes -m shell -b -a "systemctl status openstack-glance-registry openstack-glance-api"
 ansible controller_nodes -m shell -b -a "systemctl status openstack-nova-api openstack-nova-scheduler openstack-nova-conductor openstack-nova-novncproxy"
-ansible controller_nodes -m shell -b -a "systemctl status neutron-server neutron-dhcp-agent neutron-metadata-agent neutron-l3-agent neutron-linuxbridge-agent"
+ansible network_nodes -m shell -b -a "systemctl status neutron-server neutron-dhcp-agent neutron-metadata-agent neutron-l3-agent neutron-linuxbridge-agent"
 ansible compute_nodes -m shell -b -a "systemctl status openstack-nova-compute neutron-linuxbridge-agent"
