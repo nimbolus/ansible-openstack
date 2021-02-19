6a9148f01d26a756471a3c5d8b210f7a816ab66e
Lukas Steiner <lukas.steiner@steinheilig.de>
2021-02-19 18:41:21 +0100

    fix octavia mgmt interface

--- roles/octavia/files/octavia-interface.sh
+++ roles/octavia/files/octavia-interface.sh
@@ -3,17 +3,27 @@
 set -ex
 
 BRNAME=brq$(echo $MGMT_NETWORK_ID | cut -c 1-11)
+IFACE="o-bhm0"
 
 if [ "$1" == "start" ]; then
-  ip link add o-hm0 type veth peer name o-bhm0
-  brctl addif $BRNAME o-bhm0
-  ip link set o-bhm0 up
+  if ip a | grep $IFACE; then
+    echo "interface $IFACE already exists"
+  else
+    ip link add o-hm0 type veth peer name $IFACE
+  fi
+  while true; do
+    # wait for bridge to come up
+    ip a | grep "${BRNAME}:" && break
+    sleep 10
+  done
+  brctl addif $BRNAME $IFACE
+  ip link set $IFACE up
   ip link set dev o-hm0 address $MGMT_MAC_ADDRESS
   ip link set o-hm0 up
   iptables -I INPUT -i o-hm0 -p udp --dport 5555 -j ACCEPT
   dhclient -v o-hm0 -cf /etc/dhcp/octavia.conf
 elif [ "$1" == "stop" ]; then
-  brctl delif $BRNAME o-bhm0
+  brctl delif $BRNAME $IFACE
   ip link del o-hm0
 else
   brctl show $BRNAME
