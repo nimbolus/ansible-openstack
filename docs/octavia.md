9138b9f135a118c447747ad6baeda0414409c8ee
Lukas Steiner <lukas.steiner@steinheilig.de>
2021-10-15 16:00:27 +0200

    finalize octavia role

--- docs/octavia.md
+++ docs/octavia.md
@@ -1,10 +1,12 @@
 # Octavia
 
-## Create amphora image
+## Amphora image
 
+For image build process consult [amphora-image-build](https://docs.openstack.org/octavia/latest/admin/amphora-image-build.html).
+
+### Upload amphora image
 ```sh
-. octavia-openrc
-
+source octavia-openrc
 openstack image create --disk-format qcow2 --container-format bare \
   --private --tag amphora \
   --file amphora-x64-haproxy.qcow2 amphora-x64-haproxy
--- roles/octavia/files/octavia-interface.sh
+++ roles/octavia/files/octavia-interface.sh
@@ -3,29 +3,36 @@
 set -ex
 
 BRNAME=brq$(echo $MGMT_NETWORK_ID | cut -c 1-11)
-IFACE="o-bhm0"
+VETH_IFACE="$2"
+MGMT_IFACE="$3"
+
+if [ -z $VETH_IFACE || -z $MGMT_IFACE ]; then
+  echo "no veth or mgmt interface specified"
+  exit 1
+fi
 
 if [ "$1" == "start" ]; then
-  if ip a | grep $IFACE; then
-    echo "interface $IFACE already exists"
+  if ip a | grep $VETH_IFACE; then
+    echo "interface $VETH_IFACE already exists"
   else
-    ip link add o-hm0 type veth peer name $IFACE
+    ip link add $MGMT_IFACE type veth peer name $VETH_IFACE
   fi
   while true; do
     # wait for bridge to come up
     ip a | grep "${BRNAME}:" && break
     sleep 10
   done
-  brctl addif $BRNAME $IFACE
-  ip link set $IFACE up
-  ip link set dev o-hm0 address $MGMT_MAC_ADDRESS
-  ip link set o-hm0 up
-  iptables -I INPUT -i o-hm0 -p udp --dport 5555 -j ACCEPT
-  dhclient -v o-hm0 -cf /etc/dhcp/octavia.conf
+  brctl addif $BRNAME $VETH_IFACE
+  ip link set $VETH_IFACE up
+  ip link set dev $MGMT_IFACE address $MGMT_MAC_ADDRESS
+  ip link set $MGMT_IFACE up
+  iptables -I INPUT -i $MGMT_IFACE -p udp --dport 5555 -j ACCEPT
+  iptables -I FORWARD -i $BRNAME -o $BRNAME -j ACCEPT
+  dhclient -v $MGMT_IFACE -cf /etc/dhcp/octavia.conf
 elif [ "$1" == "stop" ]; then
-  brctl delif $BRNAME $IFACE
-  ip link del o-hm0
+  brctl delif $BRNAME $VETH_IFACE
+  ip link del $MGMT_IFACE
 else
   brctl show $BRNAME
-  ip a s dev o-hm0
+  ip a s dev $MGMT_IFACE
 fi
